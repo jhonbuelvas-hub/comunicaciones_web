c3814e4691964a39bab07787bb046649ab6f4ebf
jhonbuelvas-hub <jhon.buelvas@gmail.com>
2026-03-20 15:57:42 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import sqlite3
 
 app = Flask(__name__)
 
-def obtener_comunicaciones():
+def obtener_comunicaciones(filtro="todas"):
     try:
         conn = sqlite3.connect("database.db")
         cursor = conn.cursor()
 
-        cursor.execute("""
+        condicion = ""
+
+        if filtro == "recibidas":
+            condicion = "WHERE UPPER(tipo) = 'RECIBIDA'"
+        elif filtro == "enviadas":
+            condicion = "WHERE UPPER(tipo) = 'ENVIADA'"
+
+        query = f"""
             SELECT id, fecha, radicado_interno, tipo, asunto, emisor, receptor
             FROM comunicaciones
+            {condicion}
             ORDER BY fecha DESC
-        """)
+        """
 
+        cursor.execute(query)
         datos = cursor.fetchall()
         conn.close()
         return datos
@@ -22,10 +31,12 @@ def obtener_comunicaciones():
         print("ERROR:", e)
         return []
 
+
 @app.route("/")
 def index():
-    comunicaciones = obtener_comunicaciones()
-    return render_template("index.html", comunicaciones=comunicaciones)
+    filtro = request.args.get("filtro", "todas")
+    comunicaciones = obtener_comunicaciones(filtro)
+    return render_template("index.html", comunicaciones=comunicaciones, filtro=filtro)
 
 
 if __name__ == "__main__":
